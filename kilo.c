2c97a430f0a9f4afe054b56170362333b0ec0d24
ta-02 <talal.gl2004@gmail.com>
2025-01-06 01:08:34 -0500

    fixed horizontal scroll & movement

--- kilo.c
+++ kilo.c
@@ -264,12 +264,24 @@ void editorRefreshScreen() {
 }
 
 void editorMoveCursor(int key) {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch (key) {
         case ARROW_LEFT:
-            if (E.cx != 0) E.cx--;
+            if (E.cx != 0) {
+                E.cx--;
+            } else if (E.cy > 0) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
+            }
             break;
         case ARROW_RIGHT:
-            E.cx++;
+            if (row && E.cx < row->size) {
+                E.cx++;
+            } else if (row && E.cx == row->size) {
+                E.cy++;
+                E.cx = 0;
+            }
             break;
         case ARROW_UP:
             if (E.cy != 0) E.cy--;
@@ -278,6 +290,10 @@ void editorMoveCursor(int key) {
             if (E.cy < E.numrows) E.cy++;
             break;
     }
+
+    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+    int rowlen = row ? row->size : 0;
+    if (E.cx > rowlen) E.cx = rowlen;
 }
 
 void editorProcessKeypress() {
