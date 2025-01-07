269c56616fbb9b51eab4009904939bed2188a47a
ta-02 <talal.gl2004@gmail.com>
2025-01-07 00:23:02 -0500

    made end_key go to eol

--- kilo.c
+++ kilo.c
@@ -358,8 +358,9 @@ void editorProcessKeypress() {
         case HOME_KEY:
             E.cx = 0;
             break;
+
         case END_KEY:
-            E.cx = E.screencols - 1;
+            if (E.cy < E.numrows) E.cx = E.row[E.cy].size;
             break;
 
         case PAGE_UP:
