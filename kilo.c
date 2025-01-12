a11ee7edfb8a5b7000f5efd1b25d70e2dede7f98
talz-a <talal.gl2004@gmail.com>
2025-01-12 15:16:25 -0500

    implemented search functionality

--- kilo.c
+++ kilo.c
@@ -182,6 +182,20 @@ int editorRowCxToRx(erow *row, int cx) {
     return rx;
 }
 
+int editorRowRxToCx(erow *row, int rx) {
+    int cur_rx = 0;
+    int cx;
+    for (cx = 0; cx < row->size; cx++) {
+        if (row->chars[cx] == '\t') {
+            cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+        }
+        cur_rx++;
+        if (cur_rx > rx) return cx;
+    }
+
+    return cx;
+}
+
 void editorUpdateRow(erow *row) {
     int tabs = 0;
     int j;
@@ -374,6 +388,25 @@ void editorSave() {
 
     free(buf);
     editorSetStatusMessage("Cant save! I/O error: %s", strerror(errno));
+}
+
+void editorFind() {
+    char *query = editorPrompt("Search : %s (ESC to cancel)");
+    if (query == NULL) return;
+
+    int i;
+    for (i = 0; i < E.numrows; i++) {
+        erow *row = &E.row[i];
+        char *match = strstr(row->render, query);
+        if (match) {
+            E.cy = i;
+            E.cx = editorRowRxToCx(row, match - row->render);
+            E.rowoff = E.numrows;
+            break;
+        }
+    }
+
+    free(query);
 }
 
 struct abuf {
@@ -606,6 +639,10 @@ void editorProcessKeypress() {
             if (E.cy < E.numrows) E.cx = E.row[E.cy].size;
             break;
 
+        case CTRL_KEY('f'):
+            editorFind();
+            break;
+
         case BACKSPACE:
         case CTRL_KEY('h'):
         case DEL_KEY:
@@ -670,7 +707,7 @@ int main(int argc, char *argv[]) {
     initEditor();
     if (argc >= 2) editorOpen(argv[1]);
 
-    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
+    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
     while (1) {
         editorRefreshScreen();
